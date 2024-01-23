d7cadb9da90e4ae6a2849cd737f40e05834a4059
DivyaJ54 <divya.jainprof01@gmail.com>
2024-01-23 22:16:06 +0530

    index.js changed

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let element = document.getElementById('intro')
+const text = document.querySelector('texts');
 
-const result = element.innerText;
+const start = document.getElementById('start');
 
-console.log(result);
+const stop = document.getElementById('stop');
+
+
+const speechRecongnition =  window.speechRecognition || window.webkitSpeechRecognition;
+const recognition =  new speechRecongnition;
+
+recognition.lang = 'en';
+
+recognition.interimResults= false;
+
+recognition.continuous = true; 
+
+start.addEventListener('click',()=>{
+    recognition.start();
+    console.log('recognition started');
+})
+
+recognition.onresult=(e)=>{
+    text.textContent = e.results[0][0].transcript;
+    console.log(e);
+}
+
+stop.addEventListener('click',()=>{
+    recognition.stop();
+    console.log(`Recognition has stopped `);
+})
